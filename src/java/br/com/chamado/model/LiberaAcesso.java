1fef5e187196ccee79453799206722bb74f37f1a
CRC-TICWAN <CRC-TICWAN@CRC-TICWAN-PC>
2016-01-21 08:23:27 -0200

    varias alterações de acesso, faltando criar uma lista do banco das paginas do usuario para a verificação das permissões

---
+++ src/java/br/com/chamado/model/LiberaAcesso.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.chamado.model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author wandersonSantiago
+ */
+@ManagedBean(name = "liberaAcesso")
+@SessionScoped
+
+public class LiberaAcesso {
+
+    public LiberaAcesso() {
+
+    }
+
+    public Boolean alterarPaginas() {
+        return true;
+    }
+
+    public Boolean alterarSetor() {
+        return true;
+    }
+
+    public Boolean alterarUnidade() {
+        return true;
+    }
+
+    public Boolean alterarUsuario() {
+        return true;
+    }
+
+    public Boolean cadastrarPaginas() {
+        return true;
+    }
+
+    public Boolean cadastrarSetor() {
+        return true;
+    }
+
+    public Boolean permissao() {
+        return true;
+    }
+
+    public Boolean UnidadeXhtml() {
+        return true;
+    }
+
+    public Boolean cadastrarUsuarios() {
+        return true;
+    }
+
+    public Boolean chamadoAbertoCliente() {
+        return true;
+    }
+
+    public Boolean chamadoClienteManutenção() {
+        return true;
+    }
+
+    public Boolean chamadoClienteTi() {
+        return true;
+    }
+
+    public Boolean configuracao() {
+        return true;
+    }
+
+    public Boolean servicoEmail() {
+        return true;
+    }
+
+    public Boolean foneUnidades() {
+        return true;
+    }
+
+    public Boolean listaChamadoManutencao() {
+        return true;
+    }
+
+    public Boolean listaChamadoTi() {
+        return true;
+    }
+
+    public Boolean ramalSetor() {
+        return true;
+    }
+}
