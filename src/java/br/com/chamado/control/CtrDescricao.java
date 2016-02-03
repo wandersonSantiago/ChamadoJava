cd0639270249c63360775b10cded7c51b4b6a9c8
eduardoferrari01 <eduardo___ferrari@outlook.com>
2016-02-03 16:42:01 -0200

    Classe

---
+++ src/java/br/com/chamado/control/CtrDescricao.java
@@ -0,0 +1,87 @@
+package br.com.chamado.control;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import br.com.chamado.dao.DaoDescricao;
+import br.com.chamado.dao.DaoSetor;
+import br.com.chamado.model.Descricao;
+import br.com.chamado.model.Setor;
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.hibernate.HibernateException;
+
+/**
+ *
+ * @author wandersonSantiago
+ */
+@ManagedBean(name = "ctrDescricao")
+@SessionScoped
+public class CtrDescricao implements  Serializable{
+
+    private final DaoDescricao acessoHibernateDescricao;
+    private Descricao descricao;
+
+    public CtrDescricao() {
+        acessoHibernateDescricao = new DaoDescricao();
+    }
+
+    public String gravarDescrcao() {
+        try {
+            acessoHibernateDescricao.salvar(descricao);
+
+            return "index";
+        } catch (HibernateException e) {
+
+            return "index";
+        }
+    }
+
+    public String alterarDescricao(Descricao descricao) {
+        try {
+            this.descricao = descricao;
+            return "index";
+        } catch (HibernateException e) {
+            return "falha";
+        }
+    }
+   public String paginaAlterarDescricao(Descricao descricao) {
+        try {
+            this.descricao = descricao;
+            return "index";
+        } catch (HibernateException e) {
+            return "falha";
+        }
+    }
+
+    public String updateDescricao() {
+        try {
+            acessoHibernateDescricao.alterar(descricao);
+
+            return "index";
+        } catch (HibernateException e) {
+            return "falha";
+        }
+
+    }
+
+    public List carregarDescricao() {
+        try {
+
+            return acessoHibernateDescricao.carregaTudoOrdernado(Descricao.class, "nome");
+        } catch (HibernateException e) {
+            return new ArrayList();
+        }
+    }
+    
+
+}
---
+++ src/java/br/com/chamado/model/Descricao.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.chamado.model;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author CRC-TICWAN
+ */
+@ManagedBean(name = "descricao")
+@SessionScoped
+@Entity
+@Table(name = "descricao")
+public class Descricao implements Serializable {
+
+    @Id
+    @Column(unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    @Column(name = "tipo", nullable = false)
+    private int tipo;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Descricao other = (Descricao) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+}
