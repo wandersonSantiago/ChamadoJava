bc94be7ea0fca68d7b7af108ac6f47ef32861b35
CRC-TICWAN <CRC-TICWAN@CRC-TICWAN-PC>
2016-03-16 13:19:39 -0300

    aprimorando as permissões

--- src/java/br/com/chamado/dao/DaoChamadoc.java
+++ src/java/br/com/chamado/dao/DaoChamadoc.java
@@ -1,65 +1,43 @@
- package br.com.chamado.dao;
+package br.com.chamado.dao;
 
 import br.com.chamado.model.Chamadoc;
 import br.com.chamado.model.SessionContext;
 import br.com.chamado.model.Usuario;
 import java.util.List;
+
 /**
  *
- * @author WandersonSantiago 
+ * @author WandersonSantiago
  */
-public class DaoChamadoc extends DaoGenerico{
-    
-      private String hql;
-      private Usuario usuarioSessao = SessionContext.getInstance().getUsuarioLogado();
-      public List carregaChamadoOrdernado()
-      {
-        if(usuarioSessao.isTiCentral())
-        {
-           return carregaTudoOrdernado(Chamadoc.class,"id");
-        }
-        else if(usuarioSessao.isTi())
-        {
-           hql = "from Chamadoc where unidade = " + usuarioSessao.getUnidade().getId() + "AND tipoChamado = 1" + "OR codfuncsolic = " + usuarioSessao.getId();
-           
-           return carregaTudoOrdernadoUsandoHql(hql);
-        }
-        else if(usuarioSessao.isManutencao())
-        {
-            
+public class DaoChamadoc extends DaoGenerico {
+
+    private String hql;
+    private Usuario usuarioSessao = SessionContext.getInstance().getUsuarioLogado();
+
+    public List carregaChamadoOrdernado() {
+        if (usuarioSessao.isTiCentral()) {
+            return carregaTudoOrdernado(Chamadoc.class, "id");
+        } else if (usuarioSessao.isTi()) {
+            hql = "from Chamadoc where unidade = " + usuarioSessao.getUnidade().getId() + "AND tipoChamado = 1";
+
+            return carregaTudoOrdernadoUsandoHql(hql);
+        } else if (usuarioSessao.isManutencao()) {
+
             hql = "from Chamadoc where unidade = " + usuarioSessao.getUnidade().getId() + "AND tipoChamado = 2" + "OR codfuncsolic = " + usuarioSessao.getId();
             return carregaTudoOrdernadoUsandoHql(hql);
-        }
-        else
-        {
-        hql = "from Chamadoc where  codfuncsolic = " + usuarioSessao.getId();
-        return carregaTudoOrdernadoUsandoHql(hql);
-        
-        }
-      }
-      public List carregaChamadoOrdernadoChamadoTi()
-      {
-        if(usuarioSessao.isTiCentral())
-        {
-           return carregaTudoOrdernado(Chamadoc.class,"id");
-        }
-        else if(usuarioSessao.isTi())
-        {
-           hql = "from Chamadoc where unidade = " + usuarioSessao.getUnidade().getId() + "AND tipoChamado = 2" + "OR codfuncsolic = " + usuarioSessao.getId();
-           
-           return carregaTudoOrdernadoUsandoHql(hql);
-        }
-        else if(usuarioSessao.isManutencao())
-        {
-            
-            hql = "from Chamadoc where unidade = " + usuarioSessao.getUnidade().getId() + "AND tipoChamado = 2" + "OR codfuncsolic = " + usuarioSessao.getId();
+        } else {
+            hql = "from Chamadoc where  codfuncsolic = " + usuarioSessao.getId();
             return carregaTudoOrdernadoUsandoHql(hql);
+
         }
-        else
-        {
-        hql = "from Chamadoc where  codfuncsolic = " + usuarioSessao.getId();
+    }
+
+    public List carregaChamadoOrdernadoChamadoTi() {
+
+       
+        hql = "from Chamadoc where unidade = " + usuarioSessao.getUnidade().getId() + "AND tipoChamado = 2" + "OR codfuncsolic = " + usuarioSessao.getId();
+
         return carregaTudoOrdernadoUsandoHql(hql);
-        
-        }
-      }
+    }
+
 }
